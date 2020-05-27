c43e7c4b3c8670b3a124b76ad06c8fabbc3d599c
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-27 11:28:28 -0700

    Adds GET Request to Fetch Search Data from API
    This commit updates our codebase to contain a YouTube API key that then
    allows us to make a GET request to the API through JQuery to fetch
    search data.

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,19 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  $.get('https://www.googleapis.com/youtube/v3/search', {
+    part: 'snippet',
+    type: 'video',
+    key: options.key,
+    q: options.query,
+    maxResults: options.max,
+    videoEmbeddable: true
+  })
+    .done((data) => {
+      callback(data.items);
+    });
+  // .fail(function(error) {
+  //   console.error('YouTube Search: Failed to search: ', error);
+  // });
 };
 
 export default searchYouTube;
