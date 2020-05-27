cc97336375447d883a1362010f292c6e80777c27
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-26 17:34:13 -0700

    Creates Stateless Functions for App and VideoList
    This commit allows for the App.jsx file to render onto the screen. Also
    it now calls the VideoList, which was imported, so that we can work
    through the list of videos.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,8 @@
+// Import VideoList Var
+import VideoList from './VideoList.js';
+// import all the video examples
+import exampleVideoData from '../data/exampleVideoData.js';
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -10,7 +15,7 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <VideoList videos={exampleVideoData} />
       </div>
     </div>
   </div>
@@ -19,3 +24,4 @@ var App = () => (
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
+ReactDOM.render(<App />, document.getElementById('app'));
