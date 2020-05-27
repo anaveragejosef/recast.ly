8d40a3cd33a8a215fb0c28ef206565852ee83a57
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-26 21:01:10 -0700

    Connects the VideoList to the VideoListEntry
    This connect will allow for videos to be added to the videolist
    dynamically using a map function over each video.

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -3,11 +3,6 @@ import VideoListEntry from './VideoListEntry.js';
 
 var VideoList = (props) => (
   <div className="video-list">
-    {/* <div><h5><em>videoListEntry</em>{props.videos[0]}</h5></div>
-    <div><h5><em>videoListEntry</em>{props.videos[1]}</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
     {/* Create a .map function for prop.video */}
     {props.videos.map(video =>
       <VideoListEntry video={video} />
